ca674f9859b3f9a0c13e14f53a879394e05626fc
user <pixel22yuliya@gmail.com>
2024-07-11 19:12:06 +0200

    +

--- src/types/medusa.ts
+++ src/types/medusa.ts
@@ -3,7 +3,7 @@ import { Region as MedusaRegion, ProductVariant } from "@medusajs/medusa"
 export type Variant = Omit<ProductVariant, "beforeInsert">
 
 export interface Region extends Omit<MedusaRegion, "beforeInsert"> {}
-
+//yo
 export type CalculatedVariant = ProductVariant & {
   calculated_price: number
   calculated_price_type: "sale" | "default"
